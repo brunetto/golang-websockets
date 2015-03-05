0ea35f2bf2e9b53133a3be03d4f9807291ceef26
brunetto <brunetto.ziosi@gmail.com>
2015-03-05 01:09:27 +0100

    added JSON and doesn't work

--- init-gui/main.go
+++ init-gui/main.go
@@ -4,6 +4,7 @@ import (
 	"golang.org/x/net/websocket"
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func echoHandler(ws *websocket.Conn) {
@@ -12,19 +13,43 @@ func echoHandler(ws *websocket.Conn) {
 		var (
 			err error
 			n   int
+			outFile *os.File
+			t []byte
+			msg ParamStruct
 		)
 
 		receivedtext := make([]byte, 100)
 
 		n, err = ws.Read(receivedtext)
-
 		if err != nil {
 			fmt.Printf("Received: %d bytes\n", n)
 		}
 
 		s := string(receivedtext[:n])
-		t := []byte("Back: "+s)
-		fmt.Printf("Received: %d bytes: %s\n", n, s)
+		
+		fmt.Println("Received ", s)
+		
+		ws.Write([]byte(s))
+		
+		websocket.JSON.Receive(ws, &msg)
+		fmt.Printf("Received message: %+v\n", msg)
+		
+		if outFile, err = os.Create("params.conf"); err != nil {
+			t = []byte("Can't create file with err: " + err.Error())
+		}
+		
+		outString := "param1 = " + msg.Param1 + "\n" +
+					"param2 = " + msg.Param2 + "\n" + 
+					"param2 = " + msg.Param3 + "\n"
+
+		if _, err = outFile.WriteString(outString); err != nil {
+			t = []byte("Can't write to file with err: " + err.Error())
+		}
+		outFile.Close()
+		
+
+		t = []byte(`<a href="params.conf" download="params.conf" >Click to download configuration file</a>`)
+		
 		n, err = ws.Write(t)
 		if err != nil {
 			fmt.Printf("Sent: %d bytes\n", n)
@@ -32,6 +57,12 @@ func echoHandler(ws *websocket.Conn) {
 		// 	io.Copy(ws, ws)
 		fmt.Printf("Sent: %s\n", t)
 	}
+}
+
+type ParamStruct struct {
+	Param1 string
+	Param2 string
+	Param3 string
 }
 
 func main() {
