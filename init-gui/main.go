1ffe8b9fa9665af9a35b4dd21a002875a59e87a9
brunetto@spritz <brunetto.ziosi@gmail.com>
2015-03-05 12:18:57 +0100

    it works, but it needs a FIX on page reload

--- init-gui/main.go
+++ init-gui/main.go
@@ -2,60 +2,81 @@ package main
 
 import (
 	"golang.org/x/net/websocket"
-	"fmt"
+	"log"
 	"net/http"
 	"os"
 )
 
-func echoHandler(ws *websocket.Conn) {
+func (manager managerStruct) MessagesHandler(mws *websocket.Conn) {
+	// 	manager.Mws = mws
 
-	for {
+	log.Println("Start messagesHandler loop")
+	var (
+		text        string
+		receiveChan = make(chan string, 1)
+	)
+
+	go func(receiveChan chan string, mws *websocket.Conn) {
 		var (
-			err error
-			n   int
-			outFile *os.File
-			t []byte
-			msg ParamStruct
+			err          error
+			n            int
+			receivedtext = make([]byte, 100)
 		)
 
-		receivedtext := make([]byte, 100)
+		for {
+			n, err = mws.Read(receivedtext)
+			if err != nil {
+				log.Println("Received error: ", err)
+			}
+			receiveChan <- "Received " + string(receivedtext[:n])
+		}
+	}(receiveChan, mws)
 
-		n, err = ws.Read(receivedtext)
-		if err != nil {
-			fmt.Printf("Received: %d bytes\n", n)
+	for {
+		select {
+		case text = <-manager.Phone:
+			mws.Write([]byte(text))
+		case text = <-receiveChan:
+			log.Println(text)
 		}
 
-		s := string(receivedtext[:n])
-		
-		fmt.Println("Received ", s)
-		
-		ws.Write([]byte(s))
-		
-		websocket.JSON.Receive(ws, &msg)
-		fmt.Printf("Received message: %+v\n", msg)
-		
+	}
+}
+
+func (manager managerStruct) DataHandler(dws *websocket.Conn) {
+	// 	manager.Dws = dws
+	log.Println("Start dataHandler loop")
+	var (
+		err error
+		// 			n   int
+		outFile *os.File
+		text    string
+		data    ParamStruct
+	)
+
+	for {
+
+		websocket.JSON.Receive(dws, &data)
+		log.Printf("Received message: %+v\n", data)
+
 		if outFile, err = os.Create("params.conf"); err != nil {
-			t = []byte("Can't create file with err: " + err.Error())
+			text = "Can't create file with err: " + err.Error()
 		}
-		
-		outString := "param1 = " + msg.Param1 + "\n" +
-					"param2 = " + msg.Param2 + "\n" + 
-					"param2 = " + msg.Param3 + "\n"
+
+		outString := "param1 = " + data.Param1 + "\n" +
+			"param2 = " + data.Param2 + "\n" +
+			"param2 = " + data.Param3 + "\n"
 
 		if _, err = outFile.WriteString(outString); err != nil {
-			t = []byte("Can't write to file with err: " + err.Error())
+			text = "Can't write to file with err: " + err.Error()
 		}
 		outFile.Close()
-		
 
-		t = []byte(`<a href="params.conf" download="params.conf" >Click to download configuration file</a>`)
-		
-		n, err = ws.Write(t)
-		if err != nil {
-			fmt.Printf("Sent: %d bytes\n", n)
-		}
-		// 	io.Copy(ws, ws)
-		fmt.Printf("Sent: %s\n", t)
+		text = `<a href="params.conf" download="params.conf" >Click to download configuration file</a>`
+
+		manager.Phone <- text
+
+		log.Printf("Sent: %s\n", text)
 	}
 }
 
@@ -66,10 +87,22 @@ type ParamStruct struct {
 }
 
 func main() {
-	http.Handle("/echo", websocket.Handler(echoHandler))
+	var manager = managerStruct{
+		Phone: make(chan string),
+		Done:  make(chan struct{}),
+	}
+	http.Handle("/messages", websocket.Handler(manager.MessagesHandler))
+	http.Handle("/data", websocket.Handler(manager.DataHandler))
 	http.Handle("/", http.FileServer(http.Dir(".")))
 	err := http.ListenAndServe(":8085", nil)
 	if err != nil {
 		panic("Error: " + err.Error())
 	}
+}
+
+type managerStruct struct {
+	// 	Mws *websocket.Conn
+	// 	Dws *websocket.Conn
+	Phone chan string
+	Done  chan struct{}
 }
